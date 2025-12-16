1c974a7ffec6ad7866e7678807f6563b795ff5e0
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-12-16 09:43:00 -0300

    feat: add CORS support and implement kanban service integration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AllExceptionFilter } from './filters/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Column, Kanban } from './services/kanban.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('frontend');
+  protected readonly columns = signal<Column[]>([]);
+  private kanban = inject(Kanban);
+
+  ngOnInit() {
+    this.kanban.getColumnsWithCards().subscribe((columns) => {
+      console.log(columns);
+      this.columns.set(columns);
+    });
+  }
 }

049a02e9e60b23cfba4018cb8f58d5a4277b75f0
Mateus Leonardo <mateus_leonardo1997@hotmail.com>
2025-12-22 19:21:22 -0300

    fix: correct import paths for PrismaService in CardModule and CardService

--- backend/src/card/card.module.ts
+++ backend/src/card/card.module.ts
@@ -3,7 +3,7 @@ import { CardService } from './card.service';
 import { CardController } from './card.controller';
 import { ColumnModule } from 'src/column/column.module';
 import { ColumnService } from 'src/column/column.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [ColumnModule, PrismaModule],
--- backend/src/card/card.service.ts
+++ backend/src/card/card.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { ColumnService } from 'src/column/column.service';
 import { ReorderCardDto } from './dto/reorder-card-dto';
 
